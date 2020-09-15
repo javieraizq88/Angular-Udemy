4cdcb4fc5f050ee4ee81f8770459333e841f3ae9
Javiera Izquierdo M <javieraizquierdo7@gmail.com>
2020-09-15 13:14:12 -0300

    tipo de datos

--- typescript/typescript/app.js
+++ typescript/typescript/app.js
@@ -1,5 +1,20 @@
 "use strict";
 (function () {
     var mensaje = "hola";
-    console.log(mensaje);
+    var numero = 123;
+    var booleano = true; // true o false
+    var hoy = new Date();
+    var cualquiercosa;
+    cualquiercosa = mensaje;
+    cualquiercosa = numero;
+    cualquiercosa = booleano;
+    cualquiercosa = hoy;
+    var spiderman = {
+        nombre: "Peter",
+        edad: 30
+    };
+    spiderman = {
+        nombre: "Juan",
+        edad: 50
+    };
 })();
--- typescript/typescript/app.ts
+++ typescript/typescript/app.ts
@@ -1,9 +1,28 @@
 (function(){
 
-    const mensaje = "hola";
-    
-    console.log(mensaje);
-    
+    let mensaje : string = "hola";
+    let numero : number = 123; 
+    let booleano : boolean = true; // true o false
+    let hoy : Date = new Date();
+
+
+    let cualquiercosa;
+    cualquiercosa = mensaje;
+    cualquiercosa = numero;
+    cualquiercosa = booleano;
+    cualquiercosa = hoy; 
+
+
+
+    let spiderman = {
+        nombre: "Peter",
+        edad: 30
+    };
+
+    spiderman = {
+        nombre: "Juan",
+        edad: 50
+    }
 })();
 
 
